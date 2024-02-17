463aa85078b3cd06199b1e77692b2a98753b9406
scarrazza <stefano.carrazza@gmail.com>
2024-02-17 17:09:31 +0100

    adding port

--- src/qibo_client/tii.py
+++ src/qibo_client/tii.py
@@ -3,7 +3,8 @@ import os
 from .qibo_client import Client
 
 QRCCLUSTER_IP = os.environ.get("QRCCLUSTER_IP", "www.qrccluster.com")
-BASE_URL = f"http://{QRCCLUSTER_IP}/"
+QRCCLUSTER_PORT = os.environ.get("QRCCLUSTER_PORT", "80")
+BASE_URL = f"http://{QRCCLUSTER_IP}:{QRCCLUSTER_PORT}/"
 
 
 def TII(token: str) -> Client:
