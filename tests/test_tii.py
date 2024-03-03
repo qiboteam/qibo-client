f3a87d3fbea446f618e8d1796389c8ddd294c3be
Alessandro Candido <candido.ale@gmail.com>
2024-03-03 08:44:39 +0100

    test: Propagate the constant to function upgrade

--- tests/test_tii.py
+++ tests/test_tii.py
@@ -3,12 +3,15 @@ from unittest.mock import Mock, patch
 from qibo_client import tii
 
 PKG = "qibo_client.tii"
-FAKE_URL = "http://tii.url.com:1234"
 FAKE_TOKEN = "fakeToken"
 
 
-@patch(f"{PKG}.BASE_URL", FAKE_URL)
+def fake_url():
+    return "http://tii.url.com:1234"
+
+
+@patch(f"{PKG}.base_url", fake_url)
 @patch(f"{PKG}.Client.check_client_server_qibo_versions")
 def test_TII(mock_method: Mock):
     client = tii.TII(FAKE_TOKEN)
-    assert client.url == FAKE_URL
+    assert client.url == fake_url()
