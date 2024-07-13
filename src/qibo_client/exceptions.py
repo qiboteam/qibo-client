307b46f18c70a03d99a20df3fe0838b72173de13
Marco Rossi <rssmrc.11@gmail.com>
2024-07-13 15:33:57 +0200

    fix exception message

--- src/qibo_client/exceptions.py
+++ src/qibo_client/exceptions.py
@@ -27,5 +27,5 @@ class JobApiError(Exception):
     def __init__(self, status_code: int, message: str):
         self.status_code = status_code
         self.message = message
-        self.displayed_message = f"[{self.status_code} Error], {self.message}"
+        self.displayed_message = f"[{self.status_code} Error] {self.message}"
         super().__init__(self.displayed_message)
