b334d46597e09477bf6702e3292f9a24b83c2ee1
BrunoLiegiBastonLiegi <andreapapaluca@gmail.com>
2025-02-24 10:34:37 +0100

    fix: fixed return type hint for run-circuit

--- examples/run_successful_job.py
+++ examples/run_successful_job.py
@@ -6,21 +6,21 @@ import qibo
 from qibo_client import Client
 
 # create the circuit you want to run
-circuit = qibo.Circuit(17)
+circuit = qibo.Circuit(3)
 circuit.add(qibo.gates.GPI2(0, 0.0))
 circuit.add(qibo.gates.GPI2(1, 0.0))
 circuit.add(qibo.gates.GPI2(2, 0.0))
 circuit.add(qibo.gates.M(0, 1, 2))
 
-wire_names = [f"A{i}" for i in range(1, 18)]
+# wire_names = [f"A{i}" for i in range(1, 18)]
 
-circuit.wire_names = wire_names
+# circuit.wire_names = wire_names
 
 circuit.draw()
 
 # read the token from file
 token_path = Path(__file__).parent / "token.txt"
-token = token_path.read_text()
+token = token_path.read_text().replace("\n", "")
 
 # authenticate to server through the client instance
 client = Client(token)  # , url="http://localhost:8011")
